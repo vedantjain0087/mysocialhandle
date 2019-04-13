107de2143b0853a87147899a2b62d183ba2e6187
Vedant <mainvedant.vns123@gmail.com>
2019-04-13 21:07:26 +0530

    socialaccounts

---
+++ data.js
@@ -0,0 +1,85 @@
+var data = {"_id" : "5c5464fc72c2650df80d9ecf", 
+"_class" : "com.quaffmedia.model.User", 
+"socialHandle" : "@sudhanshu.g", 
+"phone" : "918800095150", 
+"name" : "Sudhanshu Gupta", 
+"photoUrl" : "http://com.mysocialhandle.images.s3.ap-south-1.amazonaws.com/5c5464fc72c2650df80d9ecf-cropped800322227457736311.jpg", 
+"registeredProviders" : [
+    "TWITTER", 
+    "FACEBOOK", 
+    "INSTAGRAM", 
+    "LINKEDIN"
+], 
+"socialAccounts" : [
+    {
+        "provider" : "TWITTER", 
+        "accountId" : "@Sudhans74546839", 
+        "connections" : "9", 
+        "displayName" : "Sudhanshu Gupta", 
+        "photoUrl" : "http://pbs.twimg.com/profile_images/961046882532257792/O5wotyyv_normal.jpg", 
+        "profileUrl" : "https://twitter.com/Sudhans74546839", 
+        "connectionsCapped" : false
+    }, 
+    {
+        "provider" : "FACEBOOK", 
+        "accountId" : "coffeewithsuds", 
+        "connections" : "0", 
+        "displayName" : "Sudhanshu Gupta", 
+        // "email" : "coffeewithsuds@yahoo.com", 
+        "photoUrl" : "https://graph.facebook.com/v3.2/10214179416492203/picture?height=100&width=100&migration_overrides=%7Boctober_2012%3Atrue%7D", 
+        "profileUrl" : "", 
+        "connectionsCapped" : false
+    }, 
+    {
+        "provider" : "INSTAGRAM", 
+        "accountId" : "sudhanshu.g1", 
+        "connections" : "28", 
+        "displayName" : "Sudhanshu Gupta", 
+        "photoUrl" : "https://scontent.cdninstagram.com/vp/8ec9682081516b1c2ffe4c1f8c52a675/5D39CE72/t51.2885-19/s150x150/36636598_811341615656804_1604088672087441408_n.jpg?_nc_ht=scontent.cdninstagram.com", 
+        "profileUrl" : "https://www.instagram.com/sudhanshu.g1/", 
+        "connectionsCapped" : false
+    }, 
+    {
+        "provider" : "LINKEDIN", 
+        "accountId" : "sudhanshukgupta", 
+        "connections" : "500", 
+        "displayName" : "Sudhanshu Gupta", 
+        "photoUrl" : "https://media.licdn.com/dms/image/C5103AQHlrB-bxtM4sQ/profile-displayphoto-shrink_100_100/0?e=1555545600&v=beta&t=RHOAdflXmqBmGQjAOH07c2b-J-JZXBHqrJq6uSKOAT4", 
+        "profileUrl" : "http://www.linkedin.com/in/sudhanshukgupta", 
+        "connectionsCapped" : true
+    }
+], 
+"profession" : "Developer", 
+"vCardStyle" : "TEMPLATE_DEFAULT", 
+"registered" : true, 
+"socialWorth" : 537, 
+"privacySettings" : {
+    "Mobile Number" : {
+        "exposed" : "true", 
+        "verified" : "true"
+    }, 
+    "Display Picture" : {
+        "exposed" : "true", 
+        "verified" : "false"
+    }, 
+    "Twitter" : {
+        "exposed" : "true", 
+        "verified" : "false"
+    }, 
+    "LinkedIn" : {
+        "exposed" : "true", 
+        "verified" : "true"
+    }, 
+    "Instagram" : {
+        "exposed" : "true", 
+        "verified" : "true"
+    }, 
+    "Facebook" : {
+        "exposed" : "true", 
+        "verified" : "true"
+    }
+}, 
+"tags" : [
+    "#india"
+]
+}
